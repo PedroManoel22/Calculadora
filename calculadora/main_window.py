dbb8c851fd348a7eca597c9f1d58c309cec627b1
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-01 09:14:21 -0300

    Update main_window.py

--- calculadora/main_window.py
+++ calculadora/main_window.py
@@ -1,13 +1,28 @@
 from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
 from typing import Any
 
+
 class MainWindow(QMainWindow):
     def __init__(self, /, parent: QWidget | None = None, *args: Any, **kwargs: Any) -> None:
         super().__init__(parent, *args, **kwargs)
 
+        # Configurando o layout básico
         self.cw = QWidget()
-        self.v_layout = QVBoxLayout()
-        self.cw.setLayout(self.v_layout)
-
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
         self.setCentralWidget(self.cw)
-      
+
+        # Título da janela
+        self.setWindowTitle("Calculadora mil grau")
+
+       
+
+    def adjustFixedSize(self):
+        # Última coisa a ser feita 
+        self.adjustSize() # Ajusta o tamanho da tabela caso o conteúdo seja maior
+        self.setFixedSize(self.width(), self.height()) # Colocando um tamanho fixo para a janela 
+    
+    def addWidgetToVLayout(self, widget: QWidget):
+        self.vLayout.addWidget(widget)
+        # self.adjustFixedSize()
+        

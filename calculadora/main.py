5935bbabc9161e5b84f1c319c04b506470155bbf
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-10 11:00:06 -0300

    Update main.py

--- calculadora/main.py
+++ calculadora/main.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QApplication
 import sys
 from main_window import MainWindow
+from display import Display
 from variables import WINDOW_ICON_PATH
 from PySide6.QtGui import QIcon
 
@@ -15,6 +16,13 @@ if __name__ == "__main__":
     icon = QIcon(str(WINDOW_ICON_PATH))
     app.setWindowIcon(icon)
     window.setWindowIcon(icon)
+
+    # Display
+    display = Display()
+    # display.setPlaceholderText("Digite algo:") # Se quiser colocar um placeholder
+    window.addWidgetToVLayout(display)
+   
+    
     
     # Executa tudo
     window.show()

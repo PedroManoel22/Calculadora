5f3d0257574713b159840224ecab79c07f92f5b6
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-15 10:22:33 -0300

    Update styles.py

--- calculadora/styles.py
+++ calculadora/styles.py
@@ -1,8 +1,9 @@
 import qdarkstyle
+from PySide6.QtWidgets import QApplication
 from variables import DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR, PRIMARY_COLOR
 
 qss = f"""
-    PushButton[cssClass="specialButton"] {{
+    PushButton[cssClass="specialButton"] {{ 
         color: #fff;
         background: {PRIMARY_COLOR};
         border-radius: 5px;
@@ -16,11 +17,13 @@ qss = f"""
         background: {DARKEST_PRIMARY_COLOR};
     }}
 """
+# houver -> quando passsar o mouse por cima do botão ele muda de cor
+# pressed -> quando prescionar o botão ele muda de cor
 
 
-def setupTheme(app):
-    # Aplicar o estilo escuro do qdarkstule
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyside6())
+def setupTheme(app: QApplication):
+    # Aplicar o estilo escuro do qdarkstyle
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt6())
 
     # Sobrepor com o QSS personalizado para estilização adicional
     app.setStyleSheet(app.styleSheet() + qss)
